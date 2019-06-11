9543dd4b5c54988efc21af5bbebf544b6204d0e3
oluwatobi balogun <tobibags19@gmail.com>
2019-06-11 23:45:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/* 'use strict';
 
  function theBeatlesPlay(players, instruments) {
     var allPlayers = [];
@@ -28,3 +28,30 @@
 
      return love;
 }
+*/
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
+}
+
+ function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
+}
+
+ function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;

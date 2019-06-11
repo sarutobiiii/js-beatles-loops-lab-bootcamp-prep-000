f97be1681a74d85c987bb8fb75b5c34640e86478
oluwatobi balogun <tobibags19@gmail.com>
2019-06-11 23:50:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
- function theBeatlesPlay(players, instruments) {
+ function theBeatlesPlay(musician, instruments) {
     var allPlayers = [];
     for (var i=0; i < players.length; i++) {
         allPlayers.push(players[i] + " plays " + instruments[i]);
